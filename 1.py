d8e46c5d56b1e0d987ad28a54adefb17ae7dd1d4
Anders Eriksson <eriksson.c.anders@gmail.com>
2021-12-03 18:17:39 +0100

    clean up day 1

--- 1.py
+++ 1.py
@@ -2,20 +2,15 @@ from sys import stdin
 from functools import reduce
 from itertools import pairwise
 
+def increment_if_increase(s, pair):
+    return s + (pair[0] < pair[1])
+
 def star1(measurements):
-    return reduce(
-        lambda s, pair: s + (pair[0] < pair[1]),
-        pairwise(measurements),
-        0
-    )
+    return reduce(increment_if_increase, pairwise(measurements), 0)
 
 def star2(measurements):
     triplets = zip(measurements, measurements[1:], measurements[2:])
-    return reduce(
-        lambda s, pair: s + (pair[0] < pair[1]),
-        pairwise(map(sum, triplets)),
-        0
-    )
+    return reduce(increment_if_increase, pairwise(map(sum, triplets)), 0)
 
 if __name__ == '__main__':
     measurements = [int(line) for line in stdin.readlines()]
